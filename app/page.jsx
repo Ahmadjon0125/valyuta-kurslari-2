c8560f36b21b7356637206ee6ad06bc3648d389c
Ahmadjon <ahmadjon@archlinux.localdomain>
2025-03-08 11:24:46 +0500

    changed max-with

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,7 @@ const date = new Date()
 export default function HomePage() {
     return (
         <main className="pt-15 bg-gray-100">
-            <div className="w-2xl mx-auto p-5 rounded bg-white rounded-2xl">
+            <div className="max-w-2xl mx-auto p-5 rounded bg-white rounded-2xl">
                 <div className="flex my-6">
                     <h2 className="font-extrabold mr-5">Курс валют</h2>
                     <p className="text-gray-400 font-bold">{ date.toLocaleDateString('ru-RU')}</p>
